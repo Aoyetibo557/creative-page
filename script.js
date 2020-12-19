3967242ce3ccce3d4e34fa873b370b7b9681b5d2
Anuoluwapo Oyetibo <Aoyetibo@gmail.com>
2020-12-19 18:25:42 -0500

    Files added 12/19/20

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(newImg){
+    document.querySelector('.cars').src = newImg;
+}
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
